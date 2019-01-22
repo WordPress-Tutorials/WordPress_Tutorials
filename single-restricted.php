751cb71260df0e3e03fae08c03f79fc1c5fe90e8
Dmytro Kosto <dmykos@gmail.com>
2019-01-22 09:11:20 +0200

    Only show restricted pages if user has dmykos_view_restricted_content capability

--- single-restricted.php
+++ single-restricted.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
- */
-
 get_header();
 ?>
 
@@ -21,34 +11,23 @@ get_header();
 			while ( have_posts() ) :
 				the_post();
 
-				get_template_part( 'template-parts/content/content', 'single' );
+                $viewRestrictedContent = "dmykos_view_restricted_content";
+                $canViewRestrictedContent = user_can( get_current_user_id(), $viewRestrictedContent );
 
-				if ( is_singular( 'attachment' ) ) {
-					// Parent post navigation.
-					the_post_navigation(
-						array(
-							/* translators: %s: parent post link */
-							'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentynineteen' ), '%title' ),
-						)
-					);
-				} elseif ( is_singular( 'post' ) ) {
-					// Previous/next post navigation.
-					the_post_navigation(
-						array(
-							'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'twentynineteen' ) . '</span> ' .
-								'<span class="screen-reader-text">' . __( 'Next post:', 'twentynineteen' ) . '</span> <br/>' .
-								'<span class="post-title">%title</span>',
-							'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'twentynineteen' ) . '</span> ' .
-								'<span class="screen-reader-text">' . __( 'Previous post:', 'twentynineteen' ) . '</span> <br/>' .
-								'<span class="post-title">%title</span>',
-						)
-					);
-				}
+                var_dump($canViewRestrictedContent);
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
+                if($canViewRestrictedContent){
+                    get_template_part( 'template-parts/content/content', 'single' );
+
+                    // If comments are open or we have at least one comment, load up the comment template.
+                    if ( comments_open() || get_comments_number() ) {
+                        comments_template();
+                    }
+                } else {
+                    ?>
+                    <h1>Content is Restricted</h1>
+                    <?php
+                }
 
 			endwhile; // End of the loop.
 			?>
